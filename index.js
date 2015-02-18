0a05af70b484d5f7d5d9f2a7e50c00437f82f639
Vladimir Pasechnik <v.pasechnik@cronagency.com>
2015-02-18 17:28:19 +0200

    1st

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-//1
-var http = require('http');
- 
-//2 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><body><h1>Hello World</h1></body></html>');
-}).listen(3000);
- 
-console.log('Server running on port 3000.');
+
+
+parsed = Baby.parse(csv);
